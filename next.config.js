e3c88b206ef598c49cc0906bed51cc70d1063f8a
yuzhiyong <yuzhiyong@newrank.cn>
2022-03-24 16:48:56 +0800

    wip: 静态文件引入地址修改

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  assetPrefix: isProd ? '/yzy-test-action-nextjs' : '',
 }
 
 module.exports = nextConfig
