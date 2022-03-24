603f6729f8d7cd08d1dde65138d5e7f49502bc53
yuzhiyong <yuzhiyong@newrank.cn>
2022-03-24 17:16:50 +0800

    wip: 配置basePath

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true, 
   assetPrefix: isProd ? '/yzy-test-action-nextjs' : '',
+  basePath:isProd? '/yzy-test-action-nextjs':'',
 }
 
 module.exports = nextConfig
