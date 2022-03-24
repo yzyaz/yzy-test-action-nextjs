6a1a98f9e14eeb75dcb79db47660da77c2ffffb3
yuzhiyong <yuzhiyong@newrank.cn>
2022-03-24 17:35:40 +0800

    wip: 去掉basePath

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true, 
-  assetPrefix: isProd ? '/yzy-test-action-nextjs' : '',
-  basePath:isProd? '/yzy-test-action-nextjs':'',
+  // assetPrefix: isProd ? '/yzy-test-action-nextjs' : '',
+  // basePath:isProd? '/yzy-test-action-nextjs':'',
 }
 
 module.exports = nextConfig
