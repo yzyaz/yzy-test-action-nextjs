9054ff4cb68b080add769350ed8f752a36a39097
yuzhiyong <yuzhiyong@newrank.cn>
2022-03-28 17:59:46 +0800

    wip: 测试

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!-pm</a>
+          Welcome to <a href="https://nextjs.org">Next.js!-pm有数111</a>
         </h1>
 
         <p className={styles.description}>
